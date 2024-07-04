5cd8876366a27e5d0f63e71994484ce0804d8ea1
stevescilar <stevemuambi@gmail.com>
2024-07-04 18:28:10 +0300

    initial commit

---
+++ expenses/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'expenses/index.html')
+
+def add_expenses(request):
+    return render(request, 'expenses/add_expenses.html')
