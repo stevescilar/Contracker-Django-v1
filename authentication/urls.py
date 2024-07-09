c9edcd0845d58af859bb61dbff0ab1c92b8662fa
stevescilar <stevemuambi@gmail.com>
2024-07-09 22:28:11 +0300

    "Added UsernameValidationView and corresponding URL pattern to authentication app"

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
-from .views import RegistrationView
+from .views import RegistrationView, UsernameValidationView
 from django.urls import path
 
 
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
+    path('validate-username',UsernameValidationView.as_view(), name='validate-username')
+
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,15 @@
+import json
 from django.shortcuts import render
 from django.views import View
+from django.http import JsonResponse
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
 
+        if str(username).isalnum():
+            return JsonResponse({'username_error': 'username should only contain alphanumeric characters'})
+        return JsonResponse({'username_valid', True})
 class RegistrationView(View):
     def get(self, request):
         return render(request, 'authentication/registration.html')
