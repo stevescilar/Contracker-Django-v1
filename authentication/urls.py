0c4848cb79d056158030b9cba0e485db46471270
stevescilar <stevemuambi@gmail.com>
2024-07-29 22:00:08 +0300

    "Updated authentication URLs: added trailing comma to 'activate' path"

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('validate-username',csrf_exempt(UsernameValidationView.as_view()), name='validate-username'),
     path('validate-email', csrf_exempt(EmailValidationView.as_view()) ,name='validate-email'),
-    path('activate',VerificationView(), name='activate'),
+    path('activate',VerificationView(), name='activate'), 
 ]
  
